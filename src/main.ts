fd54b2eaa5420ec6a457527d1992171c8a5c2bc1
josuedejesus <joshuadejesus777@gmail.com>
2026-03-07 11:44:10 -0600

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-  origin: true,
-  methods: "*",
-  allowedHeaders: "*",
-});
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
